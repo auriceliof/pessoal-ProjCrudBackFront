c3b2f7ae7915be089fc2d47c15d61862617dbf65
auriceliof <auricelio.suporte@hotmail.com>
2024-06-27 10:40:05 -0300

    Project backend created

---
+++ src/main/java/pessoal/projcrud/ProjcrudApplication.java
@@ -0,0 +1,13 @@
+package pessoal.projcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjcrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjcrudApplication.class, args);
+	}
+
+}
