94ea0dc69034525d01d6ab42644d85e2c082ae88
auriceliof <auricelio.suporte@hotmail.com>
2024-06-27 15:56:58 -0300

    Integration with H2

--- backend/src/main/java/pessoal/projcrud/controllers/StudentController.java
+++ backend/src/main/java/pessoal/projcrud/controllers/StudentController.java
@@ -1,25 +1,27 @@
 package pessoal.projcrud.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import pessoal.projcrud.entities.Student;
+import pessoal.projcrud.services.StudentService;
 
 @RestController
 @RequestMapping(value = "/students")
 public class StudentController {
+	
+	@Autowired
+	private StudentService service;
 
 	@GetMapping
 	public	ResponseEntity<List<Student>> findAll() {
-		List<Student> list = new ArrayList<>();
-			
-			list.add(new Student(1L, "Auricelio Freitas", "123.456.789-00", null, 10999.0));
-			list.add(new Student(2L, "Student Test", "123.456.789-00", null, 9999.0));
+		
+		List<Student> list = service.findAll();
 			
 		return	ResponseEntity.ok().body(list);
 	}
