915478cfb8bf8c8bd8a2a5677b3661155896d0ce
auriceliof <auricelio.suporte@hotmail.com>
2024-07-03 10:56:06 -0300

    Implemented Page Home

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,8 +1,26 @@
+import { Link } from 'react-router-dom';
+import ButtonPrimary from '../../components/ButtonPrimary';
 import './styles.css';
 
 export default function Home() {
 
     return (
-        <h1>Página Home</h1>
+        <main>
+            <section className='proj-container'>
+                <div className='proj-mt20 proj-card'>
+                    <div className="proj-home-content">
+                        <h1>Bem-vindo ao projeto CRUD no frontend!!!</h1>
+                    </div>
+                    <div className="proj-home-content">
+                        <h5>Clique no botão abaixo para iniciar</h5>
+                    </div>
+                    <div className='proj-home-btn'>
+                        <Link to="/listings">
+                            <ButtonPrimary name='Entrar'/>
+                        </Link>
+                    </div>
+                </div>            
+            </section>         
+        </main>
     )
 }
